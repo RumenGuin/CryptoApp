b6eba8a4a551e9f8fae5b3c93c66d8600c26b2ca
Rumen Guin <rumen2k@gmail.com>
2021-12-14 11:13:19 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by RUMEN GUIN on 14/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
